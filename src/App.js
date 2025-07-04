8fd08658d3d343291bea4b7af5dd9aa5bd6f9d38
muhitasenthil13@gmail.com <muhitasenthil13@gamil.com>
2025-07-04 10:00:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Counter from './counter';
 import { useState } from 'react';
 
 function App() {
   const[darkMode, setDarkMode] = useState(false);
+  const [name, setName] = useState("");
   return (
     <div className={darkMode ? "container dark" : "container"}>
       <h1>Click Counter App</h1>
-      <Counter />
+      <input
+      type="text"
+      placeholder="Enter your name:"
+      value={name}
+       onChange={(e) => setName(e.target.value)}
+      />
+      <p>Hello, {name ? name : "User"}</p>
       <button className="mode btn" onClick ={() => setDarkMode(!darkMode)}>
       {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode "}
       </button>
+        <Counter />
     </div>
   );
 }
